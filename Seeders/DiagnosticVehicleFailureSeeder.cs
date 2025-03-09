2ddae9c09c523eee792cf626d3496b756ec7a3b0
jtorresl79c <jtorresl79c@gmail.com>
2025-03-08 23:28:49 -0800

    DiagnosticVehicleFailureSeeder no comprueba si ya existe un registro, haciendo que si ya existia ocurriera un error, se soluciono

--- Seeders/DiagnosticVehicleFailureSeeder.cs
+++ Seeders/DiagnosticVehicleFailureSeeder.cs
@@ -6,9 +6,10 @@ namespace WorkshopsGov.Seeders
     {
         public static void Seed(ApplicationDbContext context)
         {
+            if (context.VehicleFailures.Any()) return;
             var diagnostic = context.Diagnostics.FirstOrDefault();
             var vehicleFailure = context.VehicleFailures.FirstOrDefault();
-        
+            
             if (diagnostic != null && vehicleFailure != null)
             {
                 diagnostic.VehicleFailures.Add(vehicleFailure);
