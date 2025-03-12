4baeccc6a8440fc6f4ae65972896c25513fdc702
codeforgemex <codeforgemex@gmail.com>
2025-03-12 16:22:38 -0700

    Logout, regsistro de usuario, nav en proceso, y vista de fallas en proceso

---
+++ Services/DummyEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace WorkshopsGov.Services;
+
+public class DummyEmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        // No hacer nada, solo para cumplir con la dependencia
+        return Task.CompletedTask;
+    }
+}
